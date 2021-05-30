0369be37598cf1d232de9575e2679acaac0b6e26
arunimajindal <72515801+arunimajindal@users.noreply.github.com>
2021-05-30 13:15:42 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "WORDBUBBLE";
+const accessCode2 = "MUSIC";
+const accessCode3 = "OCTOBER";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("B W D O B L U R B E", 100,50)
+    fill("lightblue")
+    text("Hint: What's my favourite game?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("S C M I U", 700,150)
+    fill("lightblue")
+    text("Hint: What stresses me out?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text(" T O B R C E O", 100,250)
+    fill("lightblue")
+    text("Hint:My Birthday Month", 100,270)
+
+}
